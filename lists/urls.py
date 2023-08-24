a25646c53a26255524603a599bb65c8307fb7f7c
Austin Kho <kori.susie@gmail.com>
2023-08-24 22:04:05 +0900

    url update

--- lists/urls.py
+++ lists/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, re_path
 
-from lists.views import home_page, view_list, new_list, add_item
+from lists.views import view_list, new_list, add_item
 
 urlpatterns = [
-    path('', home_page, name='home'),
-    re_path(r'^lists/(\d+)/$', view_list, name='view_list'),
-    re_path(r'^lists/(\d+)/add$', add_item, name='add_item'),
-    path('lists/new', new_list, name='new_list'),
+    re_path(r'^(\d+)/$', view_list, name='view_list'),
+    re_path(r'^(\d+)/add$', add_item, name='add_item'),
+    path('new', new_list, name='new_list'),
 ]
