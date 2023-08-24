b767c622c3196352a85416cb3150572e651841aa
Austin Kho <kori.susie@gmail.com>
2023-08-24 22:00:36 +0900

    new url and view for adding item, FT success

--- lists/urls.py
+++ lists/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 
-from lists.views import home_page, view_list, new_list
+from lists.views import home_page, view_list, new_list, add_item
 
 urlpatterns = [
     path('', home_page, name='home'),
-    path('lists/the-only-list-in-the-world/', view_list, name='view_list'),
+    re_path(r'^lists/(\d+)/$', view_list, name='view_list'),
+    re_path(r'^lists/(\d+)/add$', add_item, name='add_item'),
     path('lists/new', new_list, name='new_list'),
 ]
