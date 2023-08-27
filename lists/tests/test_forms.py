1942b590a092ca8380a2803cd6500dbf2f39c4ee
Austin Kho <kori.susie@gmail.com>
2023-08-27 22:58:18 +0900

    duplicate Validation update

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from lists.forms import ItemForm, EMPTY_LIST_ERROR
+from lists.forms import ItemForm, EMPTY_LIST_ERROR, ExistingListItemForm, DUPLICATE_ITEM_ERROR
 from lists.models import List, Item
 
 
@@ -20,6 +20,27 @@ class ItemFormTest(TestCase):
         list_ = List.objects.create()
         form = ItemForm(data={'text': 'do me'})
         add_item = form.save(for_list=list_)
-        self.assertEqual(add_item, Item.objects.first())
+        self.assertEqual(add_item, Item.objects.last())
         self.assertEqual(add_item.text, 'do me')
         self.assertEqual(add_item.list, list_)
+
+
+class ExistingListItemFormTest(TestCase):
+
+    def test_form_renders_item_text_input(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_)
+        self.assertIn('placeholder="작업 아이템 입력"', form.as_p())
+
+    def test_form_validation_for_blank_items(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [EMPTY_LIST_ERROR])
+
+    def test_form_validation_for_duplicate_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='중복 금지!')
+        form = ExistingListItemForm(for_list=list_, data={'text': '중복 금지!'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,3 +1,5 @@
+from unittest import skip
+
 from django.test import TestCase
 from django.utils.html import escape
 
@@ -97,6 +99,19 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_LIST_ERROR))
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='text1')
+        response = self.client.post(
+            '/lists/%d/' % list1.id,
+            data={'text': 'text1'}
+        )
+        expected_error = escape('이미 리스트에 해당 아이템이 있습니다.')
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
 
 class NewListTest(TestCase):
