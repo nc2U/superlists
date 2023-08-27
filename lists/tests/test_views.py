fd7e43799921e753a18b8ac968e804fcd3c65b9c
Austin Kho <kori.susie@gmail.com>
2023-08-27 12:24:26 +0900

    view update for validation model

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import resolve
+from django.utils.html import escape
 
 from lists.models import Item, List
 from lists.views import home_page
@@ -67,6 +68,18 @@ class NewListTest(TestCase):
         self.assertRedirects(
             response,
             '/lists/%d/' % new_list.id)
+
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("빈 아이템을 등록할 수 없습니다.")
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class NewItemTest(TestCase):
