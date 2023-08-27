d9ac4561e739d1c4abf33caa9ac8b80359ac6988
Austin Kho <kori.susie@gmail.com>
2023-08-27 13:16:27 +0900

    list view refectoring for new post

--- lists/urls.py
+++ lists/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, re_path
 
-from lists.views import view_list, new_list, add_item
+from lists.views import view_list, new_list
 
 urlpatterns = [
     re_path(r'^(\d+)/$', view_list, name='view_list'),
-    re_path(r'^(\d+)/add$', add_item, name='add_item'),
     path('new', new_list, name='new_list'),
 ]
