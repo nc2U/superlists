4ee5f97e92636ae0c7583138387a988cc688e3ef
Austin Kho <kori.susie@gmail.com>
2023-08-27 15:35:06 +0900

    new form for work item

---
+++ lists/tests/test_forms.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+from lists.forms import ItemForm, EMPTY_LIST_ERROR
+
+
+class ItemFormTest(TestCase):
+    
+    def test_form_item_input_has_placeholder_and_css_classes(self):
+        form = ItemForm()
+        self.assertIn('placeholder="작업 아이템 입력"', form.as_p())
+        self.assertIn('class="form-control"', form.as_p())
+
+    def test_form_validation_for_blank_items(self):
+        form = ItemForm(data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [EMPTY_LIST_ERROR])
