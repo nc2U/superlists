a3a12de670268735d1144938530140e2e365566f
Austin Kho <kori.susie@gmail.com>
2023-08-27 14:27:35 +0900

    validation from listView

--- lists/urls.py
+++ lists/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, re_path
 from lists.views import view_list, new_list
 
 urlpatterns = [
-    re_path(r'^(\d+)/$', view_list, name='view_list'),
     path('new', new_list, name='new_list'),
+    re_path(r'^(\d+)/$', view_list, name='view_list'),
 ]
